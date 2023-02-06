874208502866b4c45b52dfe3b535f206e5c7460d
Carolina Campelo Guisso <campelocg@gmail.com>
2023-02-06 16:18:08 -0300

    Swagger-UI-Express documentation

--- product/routes/index.js
+++ product/routes/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import categories from './categoriesRoutes.js';
 import products from './productsRoutes.js';
+import SwaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+
+const swaggerDocument = YAML.load('./swagger/product.yaml');
 
 const routes = (app) => {
     app.route('/').get((req, res) => {
         res.status(200).send({titulo: "E-comm Project"})
-    })
+    });
 
     app.use(
         express.json(),
         categories,
         products
-    )
+    );
+
+    app.use('/docs', SwaggerUi.serve, SwaggerUi.setup(swaggerDocument));
 }
 
 export default routes;
