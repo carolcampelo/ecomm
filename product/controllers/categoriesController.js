3ab3b9db0bebd0a38ec55a303abdf162f033c5e7
Carolina Campelo Guisso <campelocg@gmail.com>
2023-02-06 09:43:10 -0300

    CRUD Categories

---
+++ product/controllers/categoriesController.js
@@ -0,0 +1,74 @@
+import categories from "../models/category.js";
+
+class CategoryController {
+    static listCategories = (req, res) => {
+        categories.find((err, categories) => {
+            if (err) {
+                res.status(404).send({message: 'Categories not found'})
+            } else {
+                res.status(200).json(categories);
+            }
+        })
+    }
+
+    static listCategoriesById = (req, res) => {
+        const id = req.params.id;
+
+        categories.findById(id, (err, categories) => {
+            if (err) {
+                res.status(404).send({message: `${err.message} - ID Not Found.`})
+            } else {
+                res.status(200).json(categories);
+            }
+        })
+    }
+
+    static addCategories = (req, res) => {
+        let category = new categories(req.body);
+        category.save((err) => {
+            if(err){
+                res.status(401).send({message: `${err.message} - Access Denied.`})
+            } else {
+                res.status(201).send(category.toJSON())
+            }
+        })
+    }
+    
+    static updateCategories = (req, res) => {
+        const id = req.params.id;
+
+        categories.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if (err) {
+                res.status(401).send({message: `${err.message} - Access Denied.`})
+            } else {
+                res.status(200).json(categories);
+            }
+        })
+    }
+
+    static activateCategories = (req, res) => {
+        const id = req.params.id;
+
+        categories.findByIdAndUpdate(id, {$set: {"status": true}}, (err) => {
+            if (err) {
+                res.status(401).send({message: `${err.message} - Access Denied.`})
+            } else {
+                res.status(200).json(categories);
+            }
+        })
+    }
+
+    static deleteCategories = (req, res) => {
+        const id = req.params.id;
+
+        categories.findByIdAndDelete(id, (err) => {
+            if (err) {
+                res.status(401).send({message: `${err.message} - Access Denied.`})
+            } else {
+                res.status(200).json(categories);
+            }
+        })
+    }
+}
+
+export default CategoryController;
