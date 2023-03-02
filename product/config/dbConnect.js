55e02b5c77f4b8998155b7959100345d90144749
Carolina Campelo Guisso <campelocg@gmail.com>
2023-03-02 10:01:22 -0300

    Configs: supertest and jest

--- product/config/dbConnect.js
+++ product/config/dbConnect.js
@@ -1,6 +1,14 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://admin:secret@mongo-ecomm:27017/ecomm-product?authSource=admin');
+const dbConfig = {
+  url: 'mongodb://admin:secret@mongo-ecomm:27017/ecomm-product?authSource=admin',
+};
+
+const areWeTesting = process.env.JEST_WORKER_ID !== undefined;
+
+if (areWeTesting) dbConfig.url = 'mongodb://admin:secret@localhost:27017/ecomm-product?authSource=admin';
+
+mongoose.connect(dbConfig.url);
 
 const db = mongoose.connection;
 
