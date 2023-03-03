22106e3e6e7c81669b00a57753b688be431cb1aa
Carolina Campelo Guisso <campelocg@gmail.com>
2023-03-03 16:44:09 -0300

    Removing unnecessary files

--- account/test/use-case/createUserAccount.test.js
+++
@@ -1,8 +0,0 @@
-import { accounts, createUserUseCase } from '../../src/use-case/createUserAccount.js';
-
-createUserUseCase('Carolina', 'carol@email.com', 'senhaCarol123');
-createUserUseCase('Karina', 'karina@email.com', 'senhaKarina123');
-createUserUseCase('Cairo', 'cairo@email.com', 'senhaCairo123');
-createUserUseCase('Gabriel', 'gabriel@email.com', 'senhaGabriel123');
-
-console.log(accounts);
