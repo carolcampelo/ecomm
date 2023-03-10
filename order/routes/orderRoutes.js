24a79e98e984f389aeb1728c8c07f73a9d96eb63
Carolina Campelo Guisso <campelocg@gmail.com>
2023-03-10 17:36:37 -0300

    Feat: bearer auth for order

--- order/routes/orderRoutes.js
+++ order/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const OrdersController = require('../controllers/OrdersController.js');
+const authBearer = require('../middlewares/tokenAuth.js');
 
 const router = Router();
 
 router
-  .post('/orders/', OrdersController.addNewOrder)
-  .patch('/orders/:id/:status', OrdersController.updateOrderStatusByLink);
+  .post('/orders/', authBearer, OrdersController.addNewOrder)
+  .patch('/orders/:id/:status', authBearer, OrdersController.updateOrderStatusByLink);
 
 module.exports = router;
