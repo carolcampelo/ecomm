7d4c7e78aebb8e26d3064a51252ce12e2a54f1d6
Carolina Campelo Guisso <campelocg@gmail.com>
2023-03-10 15:28:35 -0300

    Feat: bearer auth for products and categories

--- product/routes/categoriesRoutes.js
+++ product/routes/categoriesRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import CategoryController from '../controllers/categoriesController.js';
+import authBearer from '../middlewares/tokenAuth.js';
 
 const router = express.Router();
 
 router
   .get('/api/categories', CategoryController.listCategories)
   .get('/api/categories/:id', CategoryController.listCategoriesById)
-  .post('/api/admin/categories', CategoryController.addCategories)
-  .put('/api/admin/categories/:id', CategoryController.updateCategories)
-  .patch('/api/admin/categories/:id', CategoryController.activateCategories)
-  .delete('/api/admin/categories/:id', CategoryController.deleteCategories);
+  .post('/api/admin/categories', authBearer, CategoryController.addCategories)
+  .put('/api/admin/categories/:id', authBearer, CategoryController.updateCategories)
+  .patch('/api/admin/categories/:id', authBearer, CategoryController.activateCategories)
+  .delete('/api/admin/categories/:id', authBearer, CategoryController.deleteCategories);
 
 export default router;
--- product/routes/productsRoutes.js
+++ product/routes/productsRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import ProductController from '../controllers/productsController.js';
+import authBearer from '../middlewares/tokenAuth.js';
 
 const router = express.Router();
 
 router
   .get('/api/products', ProductController.findProducts)
   .get('/api/products/:id', ProductController.findProductById)
-  .post('/api/admin/products', ProductController.addProducts)
-  .put('/api/admin/products/:id', ProductController.updateProducts)
-  .delete('/api/admin/products/:id', ProductController.deleteProducts);
+  .post('/api/admin/products', authBearer, ProductController.addProducts)
+  .put('/api/admin/products/:id', authBearer, ProductController.updateProducts)
+  .delete('/api/admin/products/:id', authBearer, ProductController.deleteProducts);
 
 export default router;
