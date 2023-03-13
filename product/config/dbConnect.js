9f5c5c7ba197f4a06549129c7e1594a1468d2513
Carolina Campelo Guisso <campelocg@gmail.com>
2023-03-13 10:50:04 -0300

    Fix: docker and connections

--- product/config/dbConnect.js
+++ product/config/dbConnect.js
@@ -1,16 +1,15 @@
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-console.log(DB_HOST);
+const DB_HOST = process.env.NODE_ENV === 'test' ? '127.0.0.1' : process.env.DB_HOST;
 
-const dbConfig = {
-  url: `mongodb://admin:secret@${DB_HOST}:27017/ecomm-product?authSource=admin`,
-};
+mongoose.set('strictQuery', false);
 
-mongoose.connect(dbConfig.url);
+mongoose.connect(
+  `mongodb://admin:secret@${DB_HOST}:27017/ecomm-product?authSource=admin`,
+);
 
 const db = mongoose.connection;
 
