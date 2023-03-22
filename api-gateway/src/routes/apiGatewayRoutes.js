2c5c61727aeec84fa1c5b3da0f3f3cc64be497d2
Carolina Campelo Guisso <campelocg@gmail.com>
2023-03-22 15:40:30 -0300

    Fix: proxy middleware targets

--- api-gateway/src/routes/apiGatewayRoutes.js
+++ api-gateway/src/routes/apiGatewayRoutes.js
@@ -5,15 +5,15 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 const routes = (app) => {
   
-  app.use('/api/users', createProxyMiddleware({ target: 'http://localhost:3003/', changeOrigin: true }));
-  app.use('/api/admin/users', authBearer, createProxyMiddleware({ target: 'http://localhost:3003/', changeOrigin: true }));
+  app.use('/api/users', createProxyMiddleware({ target: 'http://ecomm-account:3003/', changeOrigin: true }));
+  app.use('/api/admin/users', authBearer, createProxyMiddleware({ target: 'http://ecomm-account:3003/', changeOrigin: true }));
   
-  app.use(['/api/categories','/api/products'], createProxyMiddleware({ target: 'http://localhost:3002/', changeOrigin: true }));
-  app.use(['/api/admin/categories', '/api/admin/products'], authBearer, createProxyMiddleware({ target: 'http://localhost:3002/', changeOrigin: true }));
+  app.use(['/api/categories','/api/products'], createProxyMiddleware({ target: 'http://ecomm-product:3002/', changeOrigin: true }));
+  app.use(['/api/admin/categories', '/api/admin/products'], authBearer, createProxyMiddleware({ target: 'http://ecomm-product:3002/', changeOrigin: true }));
 
-  app.use('/payments', authBearer, createProxyMiddleware({ target: 'http://localhost:3004/', changeOrigin: true }));
+  app.use('/payments', authBearer, createProxyMiddleware({ target: 'http://ecomm-finance:3004/', changeOrigin: true }));
   
-  app.use('/orders', authBearer, createProxyMiddleware({ target: 'http://localhost:3005/', changeOrigin: true }));   
+  app.use('/orders', authBearer, createProxyMiddleware({ target: 'http://ecomm-order:3005/', changeOrigin: true }));   
     
   app.use(express.json(), routerLogin);
 
