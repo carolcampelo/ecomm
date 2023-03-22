df39478bbcec0b7e3d8c9899f9985e7e1124762a
Carolina Campelo Guisso <campelocg@gmail.com>
2023-03-22 12:56:54 -0300

    Fix: connection string parameters via dotenv

--- api-gateway/src/config/dbConfig.js
+++ api-gateway/src/config/dbConfig.js
@@ -4,11 +4,12 @@ import mongoose from 'mongoose';
 dotenv.config();
 
 const DB_HOST = process.env.NODE_ENV === 'test' ? '127.0.0.1' : process.env.DB_HOST;
+const { USER, PASSWORD, PORT } = process.env;
 
 mongoose.set('strictQuery', false);
 
 const dbConfig = {
-  url: `mongodb://admin:secret@${DB_HOST}:27017/ecomm-account?authSource=admin`,
+  url: `mongodb://${USER}:${PASSWORD}@${DB_HOST}:${PORT}/ecomm-account?authSource=admin`,
 };
 
 mongoose.connect(dbConfig.url);
